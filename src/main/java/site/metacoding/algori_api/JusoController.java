60cd2c7f05b22aa072113a0aefc49bb17c51ace1
전가은 <jeonga-eun@jeonga-eun-ui-MacBookAir.local>
2022-05-23 14:45:16 +0900

    스토어 사용방식 방법2

--- src/main/java/site/metacoding/algori_api/JusoController.java
+++ src/main/java/site/metacoding/algori_api/JusoController.java
@@ -2,8 +2,10 @@ package site.metacoding.algori_api;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class JusoController {
@@ -15,8 +17,16 @@ public class JusoController {
         return "index";
     }
 
+    // 해당 콜백이 호출되면 push 해주는 게 좋다.
     @PostMapping("/juso/callback")
     public void jusoCallback(String roadFullAddr) {
         System.out.println(roadFullAddr);
+        Store.roadFullAddr = roadFullAddr;
+    }
+
+    @CrossOrigin
+    @GetMapping("/juso/check")
+    public @ResponseBody String jusoCheck() {
+        return Store.roadFullAddr;
     }
 }
---
+++ src/main/java/site/metacoding/algori_api/Store.java
@@ -0,0 +1,7 @@
+package site.metacoding.algori_api;
+
+// Redis를 사용해야 한다. key=value 형태의 DB 혹은 세션 저장소
+// 레디스가 세션 저장소이기 때문...
+public class Store {
+    public static String roadFullAddr;
+}
