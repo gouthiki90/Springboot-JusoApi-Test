a2b38adf701e6fb87e699e0c4c4e82bc90504fbc
전가은 <jeonga-eun@jeonga-eun-ui-MacBookAir.local>
2022-05-23 14:08:41 +0900

    주소 찾기 - 베이직 로직

--- src/main/java/site/metacoding/algori_api/JusoController.java
+++ src/main/java/site/metacoding/algori_api/JusoController.java
@@ -4,19 +4,44 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class JusoController {
     
     @GetMapping("/")
-    public String index(Model model) {
-        String josoUrl = "https://www.juso.go.kr/addrlink/addrLinkUrl.do?confmKey=devU01TX0FVVEgyMDIyMDUyMzEwNDgzNTExMjYwMDA=&returnUrl=http://localhost:8080/juso/callback&resultType=4";
-        model.addAttribute("jusoUrl", josoUrl);
+    public String index() {
+
         return "index";
     }
 
     @PostMapping("/juso/callback")
     public void jusoCallback(String roadFullAddr) {
-        System.out.println(roadFullAddr);
+    }
+
+    @RequestMapping(value = "/juso/popup", method = {RequestMethod.GET, RequestMethod.POST}) // 메서드를 2개 이상 가능함
+    public String jusopopup(Model model, String roadFullAddr) {
+
+        if(roadFullAddr == null) {
+            System.out.println("버튼 눌려서 호출");
+        } else {
+            System.out.println(roadFullAddr);
+            System.out.println("도로명 주소 국가 서버에서 호출");
+        }
+        
+        String josoUrl = "https://www.juso.go.kr/addrlink/addrLinkUrl.do";
+        String confmKey = "devU01TX0FVVEgyMDIyMDUyMzEwNDgzNTExMjYwMDA=";
+        String retrunUrl = "http://localhost:8080/juso/popup";
+
+        if(roadFullAddr == null) {
+            roadFullAddr = "n";
+        }
+        model.addAttribute("roadFullAddr", roadFullAddr);
+        model.addAttribute("confmKey", confmKey);
+        model.addAttribute("returnUrl", retrunUrl);
+        model.addAttribute("resultType", "4");
+        model.addAttribute("jusoUrl", josoUrl);
+        return "jusoPopup";
     }
 }
