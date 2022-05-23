37762d905ca8fb45e31e91871f7c7339adc12a5f
전가은 <jeonga-eun@jeonga-eun-ui-MacBookAir.local>
2022-05-23 12:21:08 +0900

    주소 기초세팅 완료

---
+++ src/main/java/site/metacoding/algori_api/AlgoriApiApplication.java
@@ -0,0 +1,13 @@
+package site.metacoding.algori_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoriApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoriApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/site/metacoding/algori_api/JusoApiController.java
@@ -0,0 +1,13 @@
+package site.metacoding.algori_api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JusoApiController {
+    
+    @GetMapping("/juso")
+    public void juso() {
+        
+    }
+}
